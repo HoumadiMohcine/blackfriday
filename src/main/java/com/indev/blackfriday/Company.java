a9214cac9496a6876f90972667ddb4e61e50b6fe
houmadiMohcine <mhoumadi@sqli.com>
2019-02-08 12:15:20 +0100

    deuxième commit

--- src/main/java/com/indev/blackfriday/Company.java
+++ src/main/java/com/indev/blackfriday/Company.java
@@ -1,46 +1,100 @@
 package com.indev.blackfriday;
 
+
+
 import java.util.ArrayList;
+
+
 
 public class Company {
 
-    ArrayList<Product> stock = new ArrayList() ;
+
+
+    ArrayList<Machine> machines = new ArrayList() ;
+    ArrayList<Capsule> capsules = new ArrayList() ;
+    float fee = 0 ;
 
     public float sells(String capsule) {
-        return 0;
+        float salePrice = 0 ;
+        for(int i=0 ; i < 5 ; i++){
+            salePrice += capsules.get(i).getPrice() ;
+        }
+        salePrice = salePrice + (salePrice*0.2f) ;
+        fee = fee + salePrice*0.2f ;
+        return salePrice;
+
     }
+
+
 
     public void stock(int numberOfProducts, String type, int price) {
+
         for(int i = 0 ; i < numberOfProducts ; i++){
+
             if(type.equals("capsule")){
-                Product product = new Capsule( price) ;
-                stock.add(product) ;
-            }
-            else if(type.equals("machine")){
-                Product product = new Machine( price) ;
-                stock.add(product) ;
+
+                Capsule product = new Capsule( price) ;
+
+                capsules.add(product) ;
+
             }
 
+            else if(type.equals("machine")){
+
+                Machine product = new Machine( price) ;
+
+                machines.add(product) ;
+
+            }
+
+
+
         }
+
     }
+
+
 
     public Company to(int i) {
+
         return this;
+
     }
+
+
 
     public float computeBenefit() {
+
         return 0;
+
     }
+
+
 
     public int totalAssets() {
+
         int totalAssets = 0 ;
-        for(Product product : stock ){
+
+        for(Capsule product : capsules ){
+
             totalAssets += product.getPrice() ;
+
         }
-        return totalAssets;
+        for(Machine product : machines ){
+
+            totalAssets += product.getPrice() ;
+
+        }
+        return (int) ((int) totalAssets + fee);
+
     }
 
+
+
     public Company blackFriday() {
+
         return this;
+
     }
+
 }
